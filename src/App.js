e2aa5a4b6ad0a9b7c2d54dc1ece90dc66b6de81f
bigveloper <bigveloper@gmail.com>
2021-09-28 20:08:53 +0900

    const style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
 function App() {
-    const name = undefined;
-    return <div>{name || '리액트'}</div>;
+    const name = 'React';
+    const style = {
+        backgroundColor: 'balck',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16,
+    };
+    return <div style={style}>{name}</div>;
 }
 
 export default App;
