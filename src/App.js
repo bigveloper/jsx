c6b146de7fea6f92c408a1c3efaee8e0fbfb8daa
bigveloper <bigveloper@gmail.com>
2021-09-28 19:37:26 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
-    const name = 'Rect';
-    return <div>{name === 'React' && <h1>React 입니다.</h1>}</div>;
+    const React = '재밌다.';
+    return <div>{React === '재밌다.' ? <h1>아주재밌습니다.</h1> : <h2>솔직히 말할게요.</h2>}</div>;
 }
 
 export default App;
