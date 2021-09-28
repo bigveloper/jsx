092f067f79e83ab389850006f528adf63439562d
bigveloper <bigveloper@gmail.com>
2021-09-28 19:58:12 +0900

    -&&

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
     const React = '재밌다.';
-    return <div>{React === '재밌다.' ? <h1>아주재밌습니다.</h1> : <h2>솔직히 말할게요.</h2>}</div>;
+    return <div>{React === '재밌다.' && <h1>아주재밌습니다.</h1>}</div>;
 }
 
 export default App;
