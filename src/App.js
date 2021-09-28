4d315f4f9d5200838b53c04b9799687455206fb1
bigveloper <bigveloper@gmail.com>
2021-09-28 20:10:26 +0900

    <div style={{}}></div>

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 
 function App() {
     const name = 'React';
-    const style = {
-        backgroundColor: 'balck',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16,
-    };
-    return <div style={style}>{name}</div>;
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'balck',
+                color: 'aqua',
+                fontSize: '48px',
+                fontWeight: 'bold',
+                padding: 16,
+            }}>
+            {name}
+        </div>
+    );
 }
 
 export default App;
