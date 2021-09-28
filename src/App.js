58a8a6d678071d097c7302f8ad926bd93978261d
bigveloper <bigveloper@gmail.com>
2021-09-28 19:31:08 +0900

    &&

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
     const name = 'Rect';
-    return <div>{name === 'React' ? <h1>React 입니다.</h1> : <h2>React 아닙니다.</h2>}</div>;
+    return <div>{name === 'React' && <h1>React 입니다.</h1>}</div>;
 }
 
 export default App;
