cbbfe025a8452d98fc3a6549092f4656dad066ff
bigveloper <bigveloper@gmail.com>
2021-09-28 20:19:56 +0900

    close tag, 주석

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
     const name = 'React';
 
-    return <div className="react">{name}</div>;
+    return (
+        <>
+            <form></form>
+            <br />
+            <input />
+            <br />
+            <br />
+            {/* 주석은 이렇게 달아 줘야 하지  */}
+            // 이렇게 하면 보이고 <br />
+            <br />
+            /* 이렇게 해도 보이지 */
+        </>
+    );
 }
 
 export default App;
